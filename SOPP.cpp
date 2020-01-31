a80384c3dea56b83a200a80e98b79a59e05dee9f
aman7pro <59786025+aman7pro@users.noreply.github.com>
2020-01-31 23:52:59 +0530

    Create SOPP.cpp

---
+++ SOPP.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int sumPairs(int arr[], int n) 
+{ 
+	int sum = 0; 
+	for (int i = 0; i < n; i++) 
+  {  
+		sum = sum + (arr[i] * (2 * n)); 
+	} 
+return sum; 
+} 
+int main() 
+{ 
+	int arr[t];
+  for(int i=0;i=t;i++)
+  {
+  cin>>arr[i];
+ }
+	int n = sizeof(arr) / sizeof(arr[0]); 
+
+	cout << sumPairs(arr, n); 
+  return 0; 
+} 
